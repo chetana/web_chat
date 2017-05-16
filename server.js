e1f1dd257e6e8eda0b631ceea853daf226271171
chetana <chetana.yin@gmail.com>
2017-05-16 14:34:02 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,3 @@
-var express = require('express');
-var app = express();
-var PORT = process.env.PORT || 3000;
-
-var middleware = require('./middleware.js')
-
-
-app.use(middleware.logger);
-
-app.get('/about', middleware.requireAuthentication, function(req, res) {
-    res.send("About us");
-});
-
-app.use(express.static(__dirname + '/public'));
-
-app.listen(PORT, function () {
-    console.log("server started on port " + PORT);
-});
-
 var app = require('express')(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server),
